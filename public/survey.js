0bde5277a4ce20ae9b81c6109ac0ff071df34857
frajordo <54246966+frajordo@users.noreply.github.com>
2026-03-19 13:02:05 +0100

    Initial commit

---
+++ public/survey.js
@@ -0,0 +1,41 @@
+/* Welcome to WebprogDIG! I'd like to get to know you a bit and learn a bit about your experiences.
+
+   Please fill in your answers below. Make sure not to change the variable names or remove the `export`.
+   Since the values are all strings, they will need to be quoted. You can use backquotes (`) if you want to easily include a newline. */
+
+/*** About You ***/
+
+/* Your matriculation number */
+export const matriculationNumber = "TODO";
+
+/* Your name? */
+export const name = "TODO";
+
+/* What is your nationality? */
+export const nationality = "TODO"
+
+/* How old are you? */
+export const age = "TODO";
+
+/* What did you do before studying? What school did you go to?  */
+export const priorEducation = "TODO";
+
+/***
+ * 
+ *  About Your Experiences 
+ * 
+ * ***/
+
+/* What topic/part of the class are you most interested in? */
+export const topicInterest = "TODO";
+
+/* Do you have any prior experience with web development?
+   If so, please describe it. If not, just remove the TODO. */
+export const priorWebdev = "TODO";
+
+/* What's your favorite computer science experience?
+   E.g. a class you've taken or a project you worked on. */
+export const favCSExp = "TODO";
+
+/* Anything else you want us to know? You can leave this blank by removing the TODO. */
+export const anythingElse = "TODO";
